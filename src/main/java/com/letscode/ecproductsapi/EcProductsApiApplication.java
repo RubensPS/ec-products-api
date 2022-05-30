09689401dbd05aacab949a0a39fe4a47c6ea0eaa
RubensPS <rubens_p_souza@hotmail.com>
2022-05-30 18:32:13 -0300

    Cleaning some unnecessary annotations and imports;

--- src/main/java/com/letscode/ecproductsapi/EcProductsApiApplication.java
+++ src/main/java/com/letscode/ecproductsapi/EcProductsApiApplication.java
@@ -2,8 +2,10 @@ package com.letscode.ecproductsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EcProductsApiApplication {
 
     public static void main(String[] args) {
